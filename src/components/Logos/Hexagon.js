2a1a3c6f236741d35777974874ad177db3f560a8
raghu0891 <raghu0891@gmail.com>
2023-07-18 19:56:43 +0530

    plugin logo svg update

--- src/components/Logos/Hexagon.js
+++ src/components/Logos/Hexagon.js
@@ -1,7 +1,7 @@
 import { Logo } from 'components/Logo'
 import PropTypes from 'prop-types'
 import React from 'react'
-import src from '../../images/icon-logo-blue.svg'
+import src from '../../images/plugin-operator-logo.svg'
 
 const Hexagon = (props) => {
   return <Logo src={src} alt="Chainlink Operator" {...props} />
--- src/components/Logos/Main.js
+++ src/components/Logos/Main.js
@@ -1,7 +1,7 @@
 import { Logo } from 'components/Logo'
 import PropTypes from 'prop-types'
 import React from 'react'
-import src from '../../images/chainlink-operator-logo.svg'
+import src from '../../images/plugin-operator-logo.svg'
 
 const Main = (props) => {
   return <Logo src={src} alt="Chainlink Operator" {...props} />
